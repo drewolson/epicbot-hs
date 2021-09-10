68f9e60ce9c78f74cdf02f66c76731711ed3fd17
drew <drew@drewolson.org>
2021-09-10 08:06:06 -0500

    Small formatting fix

--- src/Epicbot/Data/Card.hs
+++ src/Epicbot/Data/Card.hs
@@ -9,7 +9,11 @@ where
 
 import Data.Text (Text)
 
-data Card = Card {externalId :: Text, name :: Text, urls :: [Text]}
+data Card = Card
+  { externalId :: Text,
+    name :: Text,
+    urls :: [Text]
+  }
   deriving (Show, Eq)
 
 new :: Text -> Text -> [Text] -> Card
